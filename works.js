6d157c536130aba8ad5d78319930e02c228b914d
Catherine Charnock <alanna.charnock@gmail.com>
2017-05-19 11:11:55 -0700

    Create works.js
    This is where I store the images so they can easily be updated without having to hunt through the html to make any updates.

---
+++ works.js
@@ -0,0 +1,22 @@
+var works = [
+	{
+	title: "UI Dashboard Design",
+	pic: "img/helmet.png",
+	url: "http://catherinealanna.com/ui-design-k-12-dashboard"
+	},
+	{
+	title: "UX App Design",
+	pic: "img/helmet.png",
+	url: "http://catherinealanna.com/educational-app-ux-design"
+	},
+	{
+	title: "Product Design",
+	pic: "img/helmet.png",
+	url: "http://catherinealanna.com/motivational-task-management-app"
+	},
+	{
+	title: "Web App Re-Design",
+	pic: "img/helmet.png",
+	url: "http://catherinealanna.com/web-app-re-design"
+	}
+	];
